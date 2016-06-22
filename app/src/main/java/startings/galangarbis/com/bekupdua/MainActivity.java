5fc176e9cda5ee0de04a9481dee22663b3ff3503
galangarbis <galangarbis@gmail.com>
2016-06-22 09:53:06 +0700

    Add ListView.xml , Listview Activity And Adapter

--- app/src/main/java/startings/galangarbis/com/bekupdua/MainActivity.java
+++ app/src/main/java/startings/galangarbis/com/bekupdua/MainActivity.java
@@ -1,13 +1,25 @@
 package startings.galangarbis.com.bekupdua;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imgview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgview = (ImageView) findViewById(R.id.imagesatu);
+        imgview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentpindah = new Intent(MainActivity.this, listActivity.class);
+                startActivity(intentpindah);
+            }
+        });
     }
 }
